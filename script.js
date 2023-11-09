39b07f7da8875c3a4e69667f6f12bd65b057683b
andresjsalcedo <134748294+andresjsalcedo@users.noreply.github.com>
2023-11-08 21:38:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -215,4 +215,36 @@ if (opcion==1){
     multiplicacion=parseInt(nume1)*parseInt(nume2);
     alert(multiplicacion);
 }
- 
+ */ conversor de unidades*/
+function convertir(valor, unidadOrigen, unidadDestino) {
+    var numero = parseFloat(valor);
+
+    var factores = {
+        "km": 1000,
+        "hm": 100,
+        "dam": 10,
+        "m": 1,
+        "dm": 0.1,
+        "cm": 0.01,
+        "mm": 0.001,
+    };
+
+    var factor = factores[unidadDestino] / factores[unidadOrigen];
+    var resultado = numero * factor;
+    return resultado;
+}
+function convertir(valor, unidadOrigen, unidadDestino) {
+    var numero = parseFloat(valor);
+
+    var factores = {
+        "km": 1000,
+        "hm": 100,
+        "dam": 10,
+        "m": 1,
+        "dm": 0.1,
+        "cm": 0.01,
+        "mm": 0.001,
+    };
+
+
+}
